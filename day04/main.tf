a2b0601954f19106c38f4c0dff8deca41f2b35db
Ayushh10 <aayush2sm@gmail.com>
2026-09-19 12:27:23 +0530

    terraform file structure

--- day04/main.tf
+++ day04/main.tf
@@ -20,12 +20,56 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "environment" {
+  default = "dev"
+}
+
+variable "channel_name" {
+  default = "ishitas3"  
+}
+
+variable "region" {
+  default = "us-east-1" 
+}
+
+locals {
+  bucket_name = "${var.channel_name}-bucket-${var.environment}"
+  vpc_name = "${var.environment}-VPC"
+}
+
 # Create a s3 bucket
 resource "aws_s3_bucket" "example" {
-  bucket = "ishitas3-sample-bucket"
+  bucket = local.bucket_name
 
   tags = {
-    Name        = "My bucket 2.0"
-    Environment = "Dev"
+    Name        = local.bucket_name
+    Environment = var.environment
   }
+}
+
+resource "aws_vpc" "sample"{
+  cidr_block = "10.0.0.0/16"
+  region = var.region
+  tags = {
+    Environment= var.environment
+    Name = local.vpc_name
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type     = "t3.micro"
+  region = var.region
+  tags = {
+    Environment= var.environment
+    Name = "${var.environment}-ec2-instance"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.sample.id
+}
+
+output "ec2_id" {
+  value = aws_instance.example.id
 }
---
+++ day04/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  default = "dev"
+}
+
+variable "channel_name" {
+  default = "ishitas3"  
+}
+
+variable "region" {
+  default = "us-east-1" 
+}
